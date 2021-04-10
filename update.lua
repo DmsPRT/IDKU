203ed36bb4535a3d92649da2f829f6428d5d1a22
DimasPrt <77457969+DmsPRT@users.noreply.github.com>
2021-04-10 18:03:25 +0700

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,5 +1,5 @@
+-- 9/04/2012
 [+] Double Click Item Inventory Untuk Use Item
 [+] Mengurangi Resource Menjadi (95 Total)
 [+] Fix Bug Beberapa Resource
-[+] Remake Denda Dan Penjara
 [+] Remake Denda Dan Penjara
